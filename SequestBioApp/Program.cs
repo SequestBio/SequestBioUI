d8440544e4b2e3060c5696b1e9560015e128d46f
hossein1001 <hossein1001@hpinc.com>
2025-06-30 17:16:28 -0500

    chaning the cards

--- SequestBioApp/Program.cs
+++ SequestBioApp/Program.cs
@@ -1,43 +1,45 @@
-using RiskCalculator.Services.RiskScore;
-using RiskCalculator.Services.Tumor;
-using SequestBioApp.Components;
+    using Radzen;
+    using RiskCalculator.Services.RiskScore;
+    using RiskCalculator.Services.Tumor;
+    using SequestBioApp.Components;
 
-var builder = WebApplication.CreateBuilder(args);
+    var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddScoped<SequestoneScoreService>();
-builder.Services.AddScoped<TumorFeatureService>();
-builder.Services.AddHttpClient();
+    builder.Services.AddScoped<SequestoneScoreService>();
+    builder.Services.AddScoped<TumorFeatureService>();
+    builder.Services.AddHttpClient();
+    builder.Services.AddRadzenComponents(); 
 
-// Add services to the container.
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
-builder.Services.AddServerSideBlazor()
-    .AddHubOptions(options =>
+    // Add services to the container.
+    builder.Services.AddRazorComponents()
+        .AddInteractiveServerComponents();
+    builder.Services.AddServerSideBlazor()
+        .AddHubOptions(options =>
+        {
+            options.MaximumReceiveMessageSize = 1000 * 1024 * 1024; // 1 GB
+        });
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (!app.Environment.IsDevelopment())
     {
-        options.MaximumReceiveMessageSize = 1000 * 1024 * 1024; // 1 GB
-    });
+        app.UseExceptionHandler("/Error", createScopeForErrors: true);
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
 
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-if (!app.Environment.IsDevelopment())
-{
     app.UseHttpsRedirection();
-}
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseHttpsRedirection();
+    }
 
 
-app.UseAntiforgery();
+    app.UseAntiforgery();
 
-app.UseStaticFiles();
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
+    app.UseStaticFiles();
+    app.MapRazorComponents<App>()
+        .AddInteractiveServerRenderMode();
 
-app.Run();
+    app.Run();
